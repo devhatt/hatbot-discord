eb3618a807f7a8b9ca72d8eedf9345f47f9fea5b
Andre Almeida <andre-silva78@hotmail.com>
2024-01-13 15:50:03 -0300

    fix pull request owner user

--- src/commands/approve/approve.ts
+++ src/commands/approve/approve.ts
@@ -10,16 +10,36 @@ export const data = new SlashCommandBuilder()
   .setDescription('fechar um Tópico sobre pr')
 
 export async function execute(interaction: CommandInteraction, client: Client) {
-  const channel = await client.channels.fetch(interaction.channelId)
+  const channel = client.channels.cache.get(interaction.channelId)
+  let pullRequestOwner: string | null = null
+
+  if (channel && channel.type === ChannelType.PublicThread) {
+    try {
+      const messages = await channel.messages.fetch({ limit: 100 })
+      const firstMessage = messages.first()
+
+      if (firstMessage) {
+        const match = firstMessage.content.match(/<@(.+?)>/)
+
+        if (match) {
+          pullRequestOwner = match[1]
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching messages:', error)
+    }
+  }
 
   if (!channel || channel.type !== ChannelType.PublicThread) {
     return
   }
 
   const members = await channel.members.fetch()
+  const pullOwner = pullRequestOwner ? members.get(pullRequestOwner) : null
 
-  const [pullOwner] = members.map((member) => member)
+  const replyContent = `❌ Necessário realizar mudanças no código, ${
+    pullOwner ? pullOwner.user : 'Unknown user'
+  }`
 
-  await interaction.reply(`✅ aprovado ${pullOwner.user}`)
-  await channel.setArchived(true)
+  await interaction.reply(replyContent)
 }
--- src/commands/change/change.ts
+++ src/commands/change/change.ts
@@ -3,61 +3,43 @@ import {
   SlashCommandBuilder,
   Client,
   ChannelType,
-  Chann
 } from 'discord.js'
 
 export const data = new SlashCommandBuilder()
   .setName('change')
   .setDescription('fechar um Tópico sobre pr')
 
-  // async function fetchAllMessages(client: Client) {
-  //   const channel = client.channels.cache.get("<my-channel-id>");
-  //   let messages = [];
-  
-  //   // Create message pointer
-  //   let message = await channel.messages
-  //     .fetch({ limit: 1 })
-  //     .then(messagePage => (messagePage.size === 1 ? messagePage.at(0) : null));
-  
-  //   while (message) {
-  //     await channel.messages
-  //       .fetch({ limit: 100, before: message.id })
-  //       .then(messagePage => {
-  //         messagePage.forEach(msg => messages.push(msg));
-  
-  //         // Update our message pointer to be the last message on the page of messages
-  //         message = 0 < messagePage.size ? messagePage.at(messagePage.size - 1) : null;
-  //       });
-  //   }
-  
-  //   console.log(messages);  // Print all messages
-  // }
-
 export async function execute(interaction: CommandInteraction, client: Client) {
   const channel = client.channels.cache.get(interaction.channelId)
+  let pullRequestOwner: string | null = null
 
+  if (channel && channel.type === ChannelType.PublicThread) {
+    try {
+      const messages = await channel.messages.fetch({ limit: 100 })
+      const firstMessage = messages.first()
 
-  if (channel) {
-    channel.messages.fetch({ limit: 100 }).then(messages => {
-      console.log(`Received ${messages.size} messages`);
-      //Iterate through the messages here with the variable "messages".
-      messages.forEach(message => console.log(message.content))
-    })
-    
+      if (firstMessage) {
+        const match = firstMessage.content.match(/<@(.+?)>/)
+
+        if (match) {
+          pullRequestOwner = match[1]
+        }
+      }
+    } catch (error) {
+      console.error('Error fetching messages:', error)
+    }
   }
-
-  console.log("🚀 ~ file: change.ts:37 ~ execute ~ channel:", channel)
-  
 
   if (!channel || channel.type !== ChannelType.PublicThread) {
     return
   }
 
   const members = await channel.members.fetch()
+  const pullOwner = pullRequestOwner ? members.get(pullRequestOwner) : null
 
-  const [pullOwner] = members.map((member) => member)
+  const replyContent = `❌ Necessário realizar mudanças no código, ${
+    pullOwner ? pullOwner.user : 'Unknown user'
+  }`
 
-  await interaction.reply(
-    `❌ Necessário realizar mudanças no código, ${pullOwner.user}`
-  )
+  await interaction.reply(replyContent)
 }
