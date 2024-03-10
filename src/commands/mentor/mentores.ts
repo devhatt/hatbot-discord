9b53ba59eeee467bbddd8bfd3897e583953767ea
Hewerton <hewertonlourenco@gmail.com>
2024-03-10 20:56:17 -0300

    feat: finishing the command mentor

--- src/commands/mentor/mentores.ts
+++ src/commands/mentor/mentores.ts
@@ -4,6 +4,7 @@ import {
   TextChannel,
   AutocompleteInteraction,
   roleMention,
+  channelMention,
 } from 'discord.js'
 
 export const data = new SlashCommandBuilder()
@@ -18,7 +19,7 @@ export const data = new SlashCommandBuilder()
   )
   .addStringOption((opt) =>
     opt
-      .setName('question')
+      .setName('pergunta')
       .setDescription('Digite a sua dúvida')
       .setRequired(true)
   )
@@ -56,13 +57,22 @@ export async function autocomplete(interaction: AutocompleteInteraction) {
 export async function execute(interaction: CommandInteraction) {
   const channel = interaction.channel as TextChannel
   const { member } = interaction
+  const channelId: string = '1128384525098033264'
 
-  const mentor = interaction.options.get('mentor-role')
-  const question = interaction.options.get('question')
+  const mentor: string = String(interaction.options.get('mentor-role')?.value)
+  const question = interaction.options.get('pergunta')
+
+  if (channel.name !== 'help') {
+    interaction.reply({
+      content: `Você não pode usar este comando aqui, utilize o canal ${channelMention(channelId)} para tal`,
+      ephemeral: true,
+    })
+    return
+  }
 
   const mentorName = await interaction.guild?.roles
-    .fetch(`${mentor?.value}`)
-    .then((role) => role?.name)
+    .fetch(`${mentor}`)
+    .then((role) => role?.name.split(' ')[0].toUpperCase())
 
   console.log(mentor)
 
@@ -73,7 +83,15 @@ export async function execute(interaction: CommandInteraction) {
   await thread.members.add(interaction.user.id)
 
   await thread.send({
-    content: `**Criado por:** ${member}\n${roleMention(`${mentor?.value}`)}`,
+    content: `**Criado por**: ${member}
+**Mentores**: ${roleMention(mentor)}
+    
+***Use o template abaixo para melhorar a sua pergunta, colocando informações que ajudem os mentores a te responder da melhor maneira possível***.
+    
+**Problema/Situação**
+**O que você já pesquisou sobre o assunto?**
+**O que você já tentou?**
+**Caso tenha, coloque uma parte do código em questão, utilize blocos de código do Discord**.`,
   })
 
   await interaction.reply({
