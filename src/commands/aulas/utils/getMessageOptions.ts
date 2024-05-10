621b1fc8e62d56eef018dab779ff6f0363ba06f8
hxsggsz <vt.hugo.2021@gmail.com>
2024-05-10 20:09:57 -0300

    refactor: remove empty space in regex

--- src/commands/aulas/utils/getMessageOptions.ts
+++ src/commands/aulas/utils/getMessageOptions.ts
@@ -1,8 +1,8 @@
 import { Message } from 'discord.js'
 
 export function getMessageOptions(message: Message<true>) {
-  const matchTitle = message.content.match(/title: \s*(.*)/)
-  const matchLink = message.content.match(/link: \s*(.*)/)
+  const matchTitle = message.content.trim().match(/title:\s*(.*)/)
+  const matchLink = message.content.trim().match(/link:\s*(.*)/)
 
   if (matchTitle && matchLink) {
     return { name: matchTitle[1], value: matchLink[1] }
